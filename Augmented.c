22015cf3dad045aee0f6e2ee5e9f2b136990e200
LarsPE <lars.emblem@gmail.com>
2022-01-15 19:51:07 +0100

    added Augmented.c

---
+++ Augmented.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[]) {
+  // augmented assignement operators =  used to replace a statement
+  //                                    where an operator takes
+  //                                    a variable as one of its arguments
+  //                                    and then assigns the result
+  //                                    back to the same variable
+  //                                    x = x + 1
+  //                                    x += 1
+
+  int x = 20;
+  
+  return 0;
+}
